7377a7338d0de5d7439b13174c9c8ac6478f0b85
Neal Probert <nprobert@probestar.net>
2020-10-20 09:50:29 -0400

    Added a file missing during generation

--- gen.sh
+++ gen.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# cmake cleanup necessary
 rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake src/*
 
+# generate C code
 mkdir -p src
 asn1c -D src -pdu=MessageFrame -fcompound-names -fline-refs -no-gen-BER -no-gen-OER -no-gen-APER -no-gen-example J2735_201603DA.ASN
 
+# missing files
+cp /usr/local/share/asn1c/asn_codecs_prim_xer.c src
